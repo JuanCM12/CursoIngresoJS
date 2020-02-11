483503b6405536de532804b3bec7c4d33c31daee
JuanCM12 <60637025+JuanCM12@users.noreply.github.com>
2020-02-11 16:52:47 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -4,8 +4,18 @@ function mostrar()
 var edad;
 edad = parseInt(document.getElementById("edad").value);
 
-if (edad >= )
+if (edad >= 18){
+    alert ("Es adulto");
+}
 
+else if (edad >= 13){
+
+    alert ("Es un adolecente");
+}
+
+else {
+    alert ("Es un niño");
+}
 
 
 }//FIN DE LA FUNCIÓN
